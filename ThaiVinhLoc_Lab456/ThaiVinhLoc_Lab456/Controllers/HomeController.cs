ac877060d3cfeae6c9f44ea8de4dd6700fc30f75
Loc <Vloc46562@gmail.com>
2020-06-19 16:00:21 +0700

    update

--- ThaiVinhLoc_Lab456/ThaiVinhLoc_Lab456/Controllers/HomeController.cs
+++ ThaiVinhLoc_Lab456/ThaiVinhLoc_Lab456/Controllers/HomeController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ThaiVinhLoc_Lab456.Models;
 
 namespace ThaiVinhLoc_Lab456.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _dbContext;
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            var upcommingCourses = _dbContext.Courses
+                .Include(c => c.Lecturer)
+                .Include(c => c.Category)
+                .Where(c => c.DateTime > DateTime.Now);
+            return View(upcommingCourses);
         }
 
         public ActionResult About()
