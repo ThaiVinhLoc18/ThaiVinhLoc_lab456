d84a01af82e63f5f97b2f38a90348983b302465f
Loc <Vloc46562@gmail.com>
2020-06-19 15:51:43 +0700

    up lai

---
+++ ThaiVinhLoc_Lab456/ThaiVinhLoc_Lab456/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ThaiVinhLoc_Lab456.Startup))]
+namespace ThaiVinhLoc_Lab456
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
